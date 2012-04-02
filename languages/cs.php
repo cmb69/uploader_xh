30cf2b1b1a4c46e773d52ffc8d145a2df0fb7f57
cmb69 <cmbecker69@gmx.de>
2012-04-02 21:07:38 +0000

    added slovak translation (contributed by Tata) added czech translations (contributed by oldnema)

---
+++ languages/cs.php
@@ -0,0 +1,30 @@
+<?php
+
+$plugin_tx['uploader']['utf-8-marker']="äöüßě";
+$plugin_tx['uploader']['menu_main']="Upload";
+$plugin_tx['uploader']['title_images']="Obrázky";
+$plugin_tx['uploader']['title_downloads']="Soubory ke stažení";
+$plugin_tx['uploader']['title_media']="Mediální soubory";
+$plugin_tx['uploader']['title_userfiles']="Uživatelské soubory";
+$plugin_tx['uploader']['label_type']="Typ";
+$plugin_tx['uploader']['label_subdir']="Podadresáře";
+$plugin_tx['uploader']['label_resize']="Změnit velikost";
+$plugin_tx['uploader']['cf_resize-small_width']="Maximální šířka pro změnu malých.";
+$plugin_tx['uploader']['cf_resize-small_height']="Maximální výška pro změnu malých";
+$plugin_tx['uploader']['cf_resize-small_quality']="Kvalita pro změnu malých.";
+$plugin_tx['uploader']['cf_resize-medium_width']="Maximální šířka pro změnu středních";
+$plugin_tx['uploader']['cf_resize-medium_height']="Maximální výška pro změnu středních";
+$plugin_tx['uploader']['cf_resize-medium_quality']="Kvalita pro změnu středních.";
+$plugin_tx['uploader']['cf_resize-large_width']="Maximální šířka pro změnu velkých.";
+$plugin_tx['uploader']['cf_resize-large_height']="Maximální výška pro změnu velkých.";
+$plugin_tx['uploader']['cf_resize-large_quality']="Kvalita pro změnu velkých.";
+$plugin_tx['uploader']['cf_size_max']="Maximální velikost souborů, které chcete vybrat. Údaj může být ve formátu 100b, 10kb, 10mb.";
+$plugin_tx['uploader']['cf_size_chunk']="Umožňuje pracovat s obrázkem v malých částech, např. pokud má PHP nastavenu maximální velikost odesílaného souboru 1MB, soubor o velikosti 10MB můžete takto rozdělit na 10 častí. Funkci můžete zrušit, pokud necháte údaj prázdný.";
+$plugin_tx['uploader']['cf_ext_images']="Uveďte čárkou oddělený seznam přípon souború, které budou uploadovany do adresáře s obrázky.";
+$plugin_tx['uploader']['cf_ext_downloads']="Uveďte čárkou oddělený seznam přípon souború, které budou uploadovany do adresáře downloads..";
+$plugin_tx['uploader']['cf_ext_media']="Uveďte čárkou oddělený seznam přípon souború, které budou uploadovány do adresáře média. (CMSimple_XH 1.5 a novější).";
+$plugin_tx['uploader']['cf_ext_userfiles']="Uveďte čárkou oddělený seznam přípon souborů, které budou uploadovany do adresáře s uživatelskými soubory.";
+$plugin_tx['uploader']['cf_runtimes']="Uveďte čárkou oddělený seznam runtin, které se mají spustit při inicializaci uploaderu. Ten se pokusí inicializovat každou rutinu, aby v případě, že některá selže, mohl pokračovat s další rutinou.";
+$plugin_tx['uploader']['cf_utf-8-marker']="Pouze pro interní použití. <strong>NEMĚNIT</strong>";
+
+?>
---
+++ languages/sk.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Dr. Martin Sereday - www.cmsimple.sk - info(at)cmsimple.sk
+ *
+ * This work is licensed under a Creative Commons Attribution 3.0 Unported
+ * License.
+ */
+
+$plugin_tx['uploader']['utf-8-marker']="äöüßľščťžýáíéňôß";
+$plugin_tx['uploader']['menu_main']="Upload";
+$plugin_tx['uploader']['title_images']="Obrázky";
+$plugin_tx['uploader']['title_downloads']="Súbory na stiahnutie";
+$plugin_tx['uploader']['title_media']="Mediálne súbory";
+$plugin_tx['uploader']['title_userfiles']="Užívateľské súbory";
+$plugin_tx['uploader']['label_type']="Typ";
+$plugin_tx['uploader']['label_subdir']="Podadresáre";
+$plugin_tx['uploader']['label_resize']="Zmeniť veľkosť";
+$plugin_tx['uploader']['cf_resize-small_width']="Maximálna šírka pre zmeny malých.";
+$plugin_tx['uploader']['cf_resize-small_height']="Maximálna výška pre zmeny malých.";
+$plugin_tx['uploader']['cf_resize-small_quality']="Kvalita pre zmeny malých.";
+$plugin_tx['uploader']['cf_resize-medium_width']="Maximálna šírka pre zmeny stredných.";
+$plugin_tx['uploader']['cf_resize-medium_height']="Maximálna výška pre zmeny stredných.";
+$plugin_tx['uploader']['cf_resize-medium_quality']="Kvalita pre zmeny stredných.";
+$plugin_tx['uploader']['cf_resize-large_width']="Maximálna šírka pre zmeny veľkých.";
+$plugin_tx['uploader']['cf_resize-large_height']="Maximálna výška pre zmeny veľkých.";
+$plugin_tx['uploader']['cf_resize-large_quality']="Kvalita pre veľké veľkých.";
+$plugin_tx['uploader']['cf_size_max']="Maximálna veľkosť súborov, ktoré chcete vybrať. Údaj môže byť vo formáte 100b, 10kb, 10mb.";
+$plugin_tx['uploader']['cf_size_chunk']="Umožňuje pracovať s obrázkom v malých častiach, napr. ak PHP má nastavenú maximálnu veľkosť odosielaného súboru 1MB, súbor o veľkosti 10MB môžete takto rozdeliť na 10 častí. Funkciu môžete zrušiť, ak necháte údaj prázdny, leave this option empty.";
+$plugin_tx['uploader']['cf_ext_images']="Uveďte čiarkou oddelený zoznam prípon súborov, ktoré budú uploadované do adresára s obrázkami.";
+$plugin_tx['uploader']['cf_ext_downloads']="Uveďte čiarkou oddelený zoznam prípon súborov, ktoré budú uploadované do adresára na stiahnutie.";
+$plugin_tx['uploader']['cf_ext_media']="Uveďte čiarkou oddelený zoznam prípon súborov, ktoré budú uploadované do adresára pre médiá. (CMSimple_XH 1.5 a novšie).";
+$plugin_tx['uploader']['cf_ext_userfiles']="Uveďte čiarkou oddelený zoznam prípon súborov, ktoré budú uploadované do adresára s užívateľskými súbormi.";
+$plugin_tx['uploader']['cf_runtimes']="Uveďte čiarkou oddelený zoznam runtin, ktoré sa majú spustiť pri inicializácii uploadera. Tento sa pokúci inicializovať každú rutinu, aby v prípade, že niektorá zlyhá, mohol pokračovať s ďalšou rutionou.";
+$plugin_tx['uploader']['cf_utf-8-marker']="Iba pre vnútorné použitie. <strong>Nič nemeňte!</strong>";
+
+?>
