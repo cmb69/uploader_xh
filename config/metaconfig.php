27ee207aabdadfbca15563986d2dfc505d98d924
Christoph M. Becker <cmbecker69@gmx.de>
2015-01-04 22:48:59 +0100

    added metaconfig.php

---
+++ config/metaconfig.php
@@ -0,0 +1,10 @@
+<?php
+
+$plugin_mcf['uploader']['resize_default']="enum:,small,medium,large";
+
+$plugin_cf['uploader']['ext_images']="jpg,gif,png";
+$plugin_cf['uploader']['ext_downloads']="pdf,zip";
+$plugin_cf['uploader']['ext_media']="flv,mp3,mp4";
+$plugin_cf['uploader']['ext_userfiles']="jpg,gif,png,pdf,zip,flv,mp3,mp4";
+
+?>
