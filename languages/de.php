979dd11ebe7c2b65a3cca92a9f555f0bb94ab426
Christoph M. Becker <cmbecker69@gmx.de>
2015-01-04 23:10:53 +0100

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,47 @@
+<?php
+
+$plugin_tx['uploader']['menu_info']="Info";
+$plugin_tx['uploader']['menu_main']="Upload";
+
+$plugin_tx['uploader']['label_type']="Typ";
+$plugin_tx['uploader']['label_subdir']="Unterordner";
+$plugin_tx['uploader']['label_resize']="Größenänderung";
+$plugin_tx['uploader']['label_expand']="Upload";
+$plugin_tx['uploader']['label_collapse']="Schließen";
+
+$plugin_tx['uploader']['title_downloads']="Downloads";
+$plugin_tx['uploader']['title_images']="Bilder";
+$plugin_tx['uploader']['title_media']="Media-Dateien";
+$plugin_tx['uploader']['title_userfiles']="Andere";
+
+$plugin_tx['uploader']['message_no_js']="JavaScript muss in Ihrem Browser aktiviert sein, um Dateien hoch laden zu können!";
+
+$plugin_tx['uploader']['syscheck_title']="Systemprüfung";
+$plugin_tx['uploader']['syscheck_phpversion']="PHP Version &ge; %s";
+$plugin_tx['uploader']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['uploader']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['uploader']['syscheck_encoding']="UTF-8 Kodierung eingestellt";
+$plugin_tx['uploader']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.org/wiki/doku.php/extend:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
+$plugin_tx['uploader']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+$plugin_tx['uploader']['cf_resize_default']="Der voreingestellte Größenänderungsmodus für Bilder: \"\" (Originalgröße), \"small\" (klein), \"medium\" (mittel) or \"large\" (groß).";
+$plugin_tx['uploader']['cf_resize-small_width']="Die maximale Breite (in Pixeln) für kleine Bilder.";
+$plugin_tx['uploader']['cf_resize-small_height']="Die maximale Höhe (in Pixeln) für kleine Bilder.";
+$plugin_tx['uploader']['cf_resize-small_quality']="Die Qualität (in Prozent) für kleine Bilder.";
+$plugin_tx['uploader']['cf_resize-medium_width']="Die maximale Breite (in Pixeln) für mittelgroße Bilder.";
+$plugin_tx['uploader']['cf_resize-medium_height']="Die maximale Höhe (in Pixeln) für mittelgroße Bilder.";
+$plugin_tx['uploader']['cf_resize-medium_quality']="Die Qualität (in Prozent) für mittelgroße Bilder.";
+$plugin_tx['uploader']['cf_resize-large_width']="Die maximale Breite (in Pixeln) für große Bilder.";
+$plugin_tx['uploader']['cf_resize-large_height']="Die maximale Höhe (in Pixeln) für große Bilder.";
+$plugin_tx['uploader']['cf_resize-large_quality']="Die Qualität (in Prozent) für große Bilder.";
+
+$plugin_tx['uploader']['cf_size_max']="Die maximale Dateigröße für den Upload. Diese kann in den folgenden Formaten angegeben werden: 100b, 10kb, 10mb.";
+$plugin_tx['uploader']['cf_size_chunk']="Ermöglicht das Stückeln von Dateien in kleinere Teile. Wenn Ihr PHP Backend z.B. eine maximale Post-Größe von 1MB hat, kann eine 10MB Datei in 10 Teile von je 1MB aufgeteilt werden. Lassen Sie diese Option leer, um das Stückeln zu deaktivieren.";
+
+$plugin_tx['uploader']['cf_ext_images']="Eine durch Kommas getrennte Liste von Dateitypen, die in den Bildordner hoch geladen werden können.";
+$plugin_tx['uploader']['cf_ext_downloads']="Eine durch Kommas getrennte Liste von Dateitypen, die in den Downloadordner hoch geladen werden können.";
+$plugin_tx['uploader']['cf_ext_media']="Eine durch Kommas getrennte Liste von Dateitypen, die in den Media-Ordner hoch geladen werden können. (Seit CMSimple_XH 1.5.)";
+$plugin_tx['uploader']['cf_ext_userfiles']="Eine durch Kommas getrennte Liste von Dateitypen, die in den allgemeinen Datei-Ordner hoch geladen werden können. (Seit CMSimple_XH 1.4.)";
+
+
+?>
