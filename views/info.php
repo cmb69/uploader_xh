93bf8d2242c8abdf390277a8f3aa046c822d800b
Christoph M. Becker <cmbecker69@gmx.de>
2017-05-09 01:45:34 +0200

    Bump copyright year

--- views/info.php
+++ views/info.php
@@ -3,7 +3,7 @@
 <img src="<?php echo self::logoPath();?>" width="128" height="128" alt="plugin
 logo" style="float: left; margin: 10px 10px 10px 0"/>
 <p>Version: <?php echo UPLOADER_VERSION;?></p>
-<p>Copyright &copy; 2011-2015 <a href="http://3-magi.net/">Christoph M. Becker</a></p>
+<p>Copyright &copy; 2011-2017 <a href="http://3-magi.net/">Christoph M. Becker</a></p>
 <p>Uploader_XH is powered by <a href="http://www.plupload.com/">Plupload</a>.</p>
 <p style="text-align: justify">This program is free software: you can
 redistribute it and/or modify it under the terms of the GNU General Public
