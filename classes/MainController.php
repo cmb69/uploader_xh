cmb69/uploader_xh

commit 95b578adce596969598034ec0ee2adb884a0c1da

Fix authorization bypass vulnerability
Currently, malicious users can bypass any constraints of the upload
widget, such as the upload type, folder, allowed extensions, etc. We
fix that by checking for all constraints on the server side, too.

classes/MainController.php
@@ -36,4 +36,47 @@ class MainController extends UploadController
         $this->subdir = $subdir;
         $this->resize = $resize;
     }
+
+    /**
+     * @return bool
+     */
+    protected function isUploadAllowed()
+    {
+        return ($this->type === '*' || $this->getType() === $this->type)
+            && ($this->subdir === '*' || $this->getSubfolder() === $this->subdir)
+            && isset($_FILES['uploader_file']['name'])
+            && $this->isExtensionAllowed($_FILES['uploader_file']['name'])
+            && isset($_FILES['uploader_file']['tmp_name'])
+            && filesize($_FILES['uploader_file']['tmp_name']) <= $this->config['size_max']
+            && $this->isSizeAllowed($_FILES['uploader_file']['tmp_name']);
+    }
+
+    /**
+     * @param string $filename
+     * @return bool
+     */
+    private function isExtensionAllowed($filename)
+    {
+        return in_array(
+            strtolower(pathinfo($filename, PATHINFO_EXTENSION)),
+            explode(',', $this->config['ext_' . $this->getType()])
+        );
+    }
+
+    /**
+     * @param string $filename
+     * @return bool
+     */
+    private function isSizeAllowed($filename)
+    {
+        $resize = $this->getResizeMode();
+        if ($resize !== '') {
+            $size = getimagesize($filename);
+            return !in_array($size[2], [IMAGETYPE_JPEG, IMAGETYPE_PNG])
+                || $size[0] <= $this->config["resize-{$resize}_width"]
+                && $size[1] <= $this->config["resize-{$resize}_height"];
+        } else {
+            return true;
+        }
+    }
 }

classes/MainAdminController.php
@@ -23,4 +23,11 @@ namespace Uploader;
 
 class MainAdminController extends UploadController
 {
+    /**
+     * @return bool
+     */
+    protected function isUploadAllowed()
+    {
+        return true;
+    }
 }

classes/UploadController.php
@@ -36,7 +36,7 @@ class UploadController
     /**
      * @var array
      */
-    private $config;
+    protected $config;
 
     /**
      * @var array
@@ -187,7 +187,7 @@ class UploadController
         ) {
             return $subdir;
         } else {
-            return '';
+            return '/';
         }
     }
 
@@ -245,7 +245,7 @@ class UploadController
         $config = array(
             'url' => (string) $url,
             'filters' => [
-                'max_file_size' => $this->config['size_max'],
+                'max_file_size' => "{$this->config['size_max']}b",
                 'mime_types' => [[
                     'title' => $this->lang['title_' . $type],
                     'extensions' => $this->config['ext_' . $type]
@@ -270,7 +270,7 @@ class UploadController
     {
         global $pth;
 
-        if (self::$serial != $_GET['uploader_serial']) {
+        if (self::$serial != $_GET['uploader_serial'] || !$this->isUploadAllowed()) {
             return;
         }
         $dir = $pth['folder'][$this->getType()] . $this->getSubfolder();

config/config.php
@@ -11,7 +11,7 @@ $plugin_cf['uploader']['resize-large_width']="800";
 $plugin_cf['uploader']['resize-large_height']="800";
 $plugin_cf['uploader']['resize-large_quality']="85";
 
-$plugin_cf['uploader']['size_max']="200mb";
+$plugin_cf['uploader']['size_max']="200000000";
 
 $plugin_cf['uploader']['ext_images']="jpg,gif,png";
 $plugin_cf['uploader']['ext_downloads']="pdf,zip";

languages/cs.php
@@ -19,7 +19,6 @@ $plugin_tx['uploader']['cf_resize-medium_quality']="Kvalita pro změnu střední
 $plugin_tx['uploader']['cf_resize-large_width']="Maximální šířka pro změnu velkých.";
 $plugin_tx['uploader']['cf_resize-large_height']="Maximální výška pro změnu velkých.";
 $plugin_tx['uploader']['cf_resize-large_quality']="Kvalita pro změnu velkých.";
-$plugin_tx['uploader']['cf_size_max']="Maximální velikost souborů, které chcete vybrat. Údaj může být ve formátu 100b, 10kb, 10mb.";
 $plugin_tx['uploader']['cf_ext_images']="Uveďte čárkou oddělený seznam přípon souború, které budou uploadovany do adresáře s obrázky.";
 $plugin_tx['uploader']['cf_ext_downloads']="Uveďte čárkou oddělený seznam přípon souború, které budou uploadovany do adresáře downloads..";
 $plugin_tx['uploader']['cf_ext_media']="Uveďte čárkou oddělený seznam přípon souború, které budou uploadovány do adresáře média. (CMSimple_XH 1.5 a novější).";

languages/da.php
@@ -25,7 +25,6 @@ $plugin_tx['uploader']['cf_resize-medium_quality']="Kvaliteten til mellemstore r
 $plugin_tx['uploader']['cf_resize-large_width']="Den maksimale bredde for store resizing.";
 $plugin_tx['uploader']['cf_resize-large_height']="Den maksimale højde for store resizing.";
 $plugin_tx['uploader']['cf_resize-large_quality']="Kvaliteten for store resizing.";
-$plugin_tx['uploader']['cf_size_max']="Maksimal filstørrelse, som du kan vælge. Denne streng kan være i følgende formater 100b og 10kb og 10 MB ..";
 $plugin_tx['uploader']['cf_ext_images']="En kommasepareret liste over filtypenavne du kan uploade til mappen Billeder.";
 $plugin_tx['uploader']['cf_ext_downloads']="En kommasepareret liste over filtypenavne du kan uploade til mappen Downloads";
 $plugin_tx['uploader']['cf_ext_media']="En kommasepareret liste over filtypenavne du kan uploade til medierne mappe (CMSimple_XH 1,5 og op).";

languages/de.php
@@ -44,7 +44,7 @@ $plugin_tx['uploader']['cf_resize-large_width']="Die maximale Breite (in Pixeln)
 $plugin_tx['uploader']['cf_resize-large_height']="Die maximale Höhe (in Pixeln) für große Bilder.";
 $plugin_tx['uploader']['cf_resize-large_quality']="Die Qualität (in Prozent) für große Bilder.";
 
-$plugin_tx['uploader']['cf_size_max']="Die maximale Dateigröße für den Upload. Diese kann in den folgenden Formaten angegeben werden: 100b, 10kb, 10mb.";
+$plugin_tx['uploader']['cf_size_max']="Die maximale Dateigröße für den Upload in Bytes.";
 
 $plugin_tx['uploader']['cf_ext_images']="Eine durch Kommas getrennte Liste von Dateitypen, die in den Bildordner hoch geladen werden können.";
 $plugin_tx['uploader']['cf_ext_downloads']="Eine durch Kommas getrennte Liste von Dateitypen, die in den Downloadordner hoch geladen werden können.";

languages/en.php
@@ -44,7 +44,7 @@ $plugin_tx['uploader']['cf_resize-large_width']="The maximum width (in pixels) f
 $plugin_tx['uploader']['cf_resize-large_height']="The maximum height (in pixels) for large resizing.";
 $plugin_tx['uploader']['cf_resize-large_quality']="The quality (in percent) for large resizing.";
 
-$plugin_tx['uploader']['cf_size_max']="Maximum file size that you can pick. This string can be in the following formats 100b, 10kb, 10mb.";
+$plugin_tx['uploader']['cf_size_max']="Maximum file size in bytes that you can pick.";
 
 $plugin_tx['uploader']['cf_ext_images']="A comma separated list of file extensions you can upload to the images folder.";
 $plugin_tx['uploader']['cf_ext_downloads']="A comma separated list of file extensions you can upload to the downloads folder.";

commit 2522ca585beefd67659505c814615cb0d2b96d2d

Fix chunked front-end upload
95b578a broke the chunked front-end upload, because in this case the
supplied filename is just "blob". We have to check the posted `name`
instead.

classes/MainController.php
@@ -44,8 +44,8 @@ class MainController extends UploadController
     {
         return ($this->type === '*' || $this->getType() === $this->type)
             && ($this->subdir === '*' || $this->getSubfolder() === $this->subdir)
-            && isset($_FILES['uploader_file']['name'])
-            && $this->isExtensionAllowed($_FILES['uploader_file']['name'])
+            && isset($_POST['name'])
+            && $this->isExtensionAllowed($_POST['name'])
             && isset($_FILES['uploader_file']['tmp_name'])
             && filesize($_FILES['uploader_file']['tmp_name']) <= $this->config['size_max']
             && $this->isSizeAllowed($_FILES['uploader_file']['tmp_name']);