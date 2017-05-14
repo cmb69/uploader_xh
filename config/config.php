93cb7d41e457a76118584f63ac8e4b7b9797d0bc
Christoph M. Becker <cmbecker69@gmx.de>
2017-05-14 15:03:44 +0200

    Use saner config defaults

--- config/config.php
+++ config/config.php
@@ -1,17 +1,17 @@
 <?php
 
 $plugin_cf['uploader']['resize_default']="";
-$plugin_cf['uploader']['resize-small_width']="200";
-$plugin_cf['uploader']['resize-small_height']="200";
-$plugin_cf['uploader']['resize-small_quality']="85";
-$plugin_cf['uploader']['resize-medium_width']="400";
-$plugin_cf['uploader']['resize-medium_height']="400";
-$plugin_cf['uploader']['resize-medium_quality']="85";
-$plugin_cf['uploader']['resize-large_width']="800";
-$plugin_cf['uploader']['resize-large_height']="800";
-$plugin_cf['uploader']['resize-large_quality']="85";
+$plugin_cf['uploader']['resize-small_width']="256";
+$plugin_cf['uploader']['resize-small_height']="256";
+$plugin_cf['uploader']['resize-small_quality']="75";
+$plugin_cf['uploader']['resize-medium_width']="512";
+$plugin_cf['uploader']['resize-medium_height']="512";
+$plugin_cf['uploader']['resize-medium_quality']="75";
+$plugin_cf['uploader']['resize-large_width']="1024";
+$plugin_cf['uploader']['resize-large_height']="1024";
+$plugin_cf['uploader']['resize-large_quality']="75";
 
-$plugin_cf['uploader']['size_max']="200000000";
+$plugin_cf['uploader']['size_max']="20000000";
 
 $plugin_cf['uploader']['ext_images']="jpg,gif,png";
 $plugin_cf['uploader']['ext_downloads']="pdf,zip";
