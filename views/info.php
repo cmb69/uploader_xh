4f32f9b90b5845e30ba3f6f4faf5e6ae0f02c684
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-05 14:00:14 +0100

    Indent XML and PHTML files with two spaces

--- views/info.php
+++ views/info.php
@@ -3,29 +3,29 @@
 <p>Version: <?=$this->version()?></p>
 <p>Copyright © 2011-2017 Christoph M. Becker</p>
 <p>
-    Uploader_XH is powered by <a target="_blank"
-    href="http://www.plupload.com/">Plupload</a>.
+  Uploader_XH is powered by <a target="_blank"
+  href="http://www.plupload.com/">Plupload</a>.
 </p>
 <p class="uploader_license">
-    Uploader_XH is free software: you can redistribute it and/or modify it under
-    the terms of the GNU General Public License as published by the Free
-    Software Foundation, either version 3 of the License, or (at your option)
-    any later version.
+  Uploader_XH is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option)
+  any later version.
 </p>
 <p class="uploader_license">
-    Uploader_XH is distributed in the hope that it will be useful, but
-    <em>without any warranty</em>; without even the implied warranty of
-    <em>merchantability</em> or <em>fitness for a particular purpose</em>. See
-    the GNU General Public License for more details.
+  Uploader_XH is distributed in the hope that it will be useful, but
+  <em>without any warranty</em>; without even the implied warranty of
+  <em>merchantability</em> or <em>fitness for a particular purpose</em>. See
+  the GNU General Public License for more details.
 </p>
 <p class="uploader_license">
-    You should have received a copy of the GNU General Public License along with
-    Uploader_XH. If not, see <a target="_blank"
-    href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+  You should have received a copy of the GNU General Public License along with
+  Uploader_XH. If not, see <a target="_blank"
+  href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
 </p>
 <div class="uploader_syscheck">
-    <h2><?php echo $this->text('syscheck_title')?></h2>
+  <h2><?php echo $this->text('syscheck_title')?></h2>
 <?php foreach ($this->checks as $check):?>
-    <p class="xh_<?php echo $this->escape($check->state)?>"><?php echo $this->text('syscheck_message', $check->label, $check->stateLabel)?></p>
+  <p class="xh_<?php echo $this->escape($check->state)?>"><?php echo $this->text('syscheck_message', $check->label, $check->stateLabel)?></p>
 <?php endforeach?>
 </div>
--- views/widget.php
+++ views/widget.php
@@ -1,43 +1,43 @@
 <div class="uploader_widget" data-config="<?=$this->pluploadConfig()?>">
-	<div class="uploader_controls">
+  <div class="uploader_controls">
 <?php if (!empty($this->typeOptions)):?>
-		<select class="uploader_type" title="<?=$this->text('label_type')?>" data-url="<?=$this->typeSelectChangeUrl()?>">
-<?php 	foreach ($this->typeOptions as $type => $selected):?>
-			<option value="<?=$this->escape($type)?>" <?=$this->escape($selected)?>><?=$this->escape($type)?></option>
-<?php 	endforeach?>
-		</select>
+    <select class="uploader_type" title="<?=$this->text('label_type')?>" data-url="<?=$this->typeSelectChangeUrl()?>">
+<?php     foreach ($this->typeOptions as $type => $selected):?>
+      <option value="<?=$this->escape($type)?>" <?=$this->escape($selected)?>><?=$this->escape($type)?></option>
+<?php     endforeach?>
+    </select>
 <?php endif?>
 <?php if (!empty($this->subdirOptions)):?>
-		<select class="uploader_subdir" title="<?=$this->text('label_subdir')?>" data-url="<?=$this->subdirSelectChangeUrl()?>">
-<?php 	foreach ($this->subdirOptions as $subdir => $selected):?>
-			<option value="<?=$this->escape($subdir)?>" <?=$this->escape($selected)?>><?=$this->escape($subdir)?></option>
-<?php	endforeach?>
-		</select>
+    <select class="uploader_subdir" title="<?=$this->text('label_subdir')?>" data-url="<?=$this->subdirSelectChangeUrl()?>">
+<?php     foreach ($this->subdirOptions as $subdir => $selected):?>
+      <option value="<?=$this->escape($subdir)?>" <?=$this->escape($selected)?>><?=$this->escape($subdir)?></option>
+<?php     endforeach?>
+    </select>
 <?php endif?>
 <?php if (!empty($this->resizeOptions)):?>
-		<select class="uploader_resize" title="<?=$this->text('label_resize')?>" data-url="<?=$this->resizeSelectChangeUrl()?>">
-<?php 	foreach ($this->resizeOptions as $size => $selected):?>
-			<option value="<?=$this->escape($size)?>" <?=$this->escape($selected)?>><?=$this->escape($size)?></option>
-<?php 	endforeach?>
-		</select>
+    <select class="uploader_resize" title="<?=$this->text('label_resize')?>" data-url="<?=$this->resizeSelectChangeUrl()?>">
+<?php     foreach ($this->resizeOptions as $size => $selected):?>
+      <option value="<?=$this->escape($size)?>" <?=$this->escape($selected)?>><?=$this->escape($size)?></option>
+<?php     endforeach?>
+    </select>
 <?php endif?>
-	</div>
-	<table class="uploader_filelist">
-		<tr>
-			<th><?=$this->text('label_filename')?></th>
-			<th><?=$this->text('label_size')?></th>
-			<th><?=$this->text('label_state')?></th>
-			<th></th>
-		</tr>
-		<tr class="uploader_row_template">
-			<td class="uploader_filename"></td>
-			<td class="uploader_size"></td>
-			<td class="uploader_progress"></td>
-			<td><button class="uploader_remove"><?=$this->text('label_remove')?></button></td>
-		</tr>
-	</table>
-	<div class="uploader_buttons">
-		<button class="uploader_pickfiles"><?=$this->text('label_select_files')?></button>
-		<button class="uploader_uploadfiles"><?=$this->text('label_upload_files')?></button>
-	</div>
+  </div>
+  <table class="uploader_filelist">
+    <tr>
+      <th><?=$this->text('label_filename')?></th>
+      <th><?=$this->text('label_size')?></th>
+      <th><?=$this->text('label_state')?></th>
+      <th></th>
+    </tr>
+    <tr class="uploader_row_template">
+      <td class="uploader_filename"></td>
+      <td class="uploader_size"></td>
+      <td class="uploader_progress"></td>
+      <td><button class="uploader_remove"><?=$this->text('label_remove')?></button></td>
+    </tr>
+  </table>
+  <div class="uploader_buttons">
+    <button class="uploader_pickfiles"><?=$this->text('label_select_files')?></button>
+    <button class="uploader_uploadfiles"><?=$this->text('label_upload_files')?></button>
+  </div>
 </div>
