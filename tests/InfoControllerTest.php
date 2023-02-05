ea042ec67c64ecc3b93e6b580e4b6a3a06ade4e1
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-05 17:59:27 +0100

    Test InfoController

---
+++ tests/InfoControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright 2023 Christoph M. Becker
+ *
+ * This file is part of Uploader_XH.
+ *
+ * Uploader_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Uploader_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Uploader_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Uploader;
+
+use ApprovalTests\Approvals;
+use PHPUnit\Framework\TestCase;
+
+class InfoControllerTest extends TestCase
+{
+    public function testIt(): void
+    {
+        $plugin_tx = XH_includeVar("./languages/en.php", 'plugin_tx');
+        $lang = $plugin_tx['uploader'];
+        $systemChecker = $this->createStub(SystemChecker::class);
+        $systemChecker->method('checkVersion')->willReturn(true);
+        $systemChecker->method('checkExtension')->willReturn(true);
+        $systemChecker->method('checkPlugin')->willReturn(true);
+        $systemChecker->method('checkWritability')->willReturn(true);
+        $sut = new InfoController("../", $lang, $systemChecker);
+        $response = $sut->defaultAction();
+        Approvals::verifyHtml($response);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright 2023 Christoph M. Becker
+ *
+ * This file is part of Uploader_XH.
+ *
+ * Uploader_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Uploader_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Uploader_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const CMSIMPLE_XH_VERSION = "CMSimple_XH 1.7.5";
+
+require_once "../../cmsimple/functions.php";
+
+require_once "./classes/InfoController.php";
+require_once "./classes/Plugin.php";
+require_once "./classes/SystemChecker.php";
+require_once "./classes/View.php";
