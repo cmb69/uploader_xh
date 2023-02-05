a06a0ccfaabb24eb8026930963065cdbbfc517c3
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-05 17:19:35 +0100

    Avoid compact()

--- classes/SystemCheckService.php
+++ classes/SystemCheckService.php
@@ -70,9 +70,11 @@ class SystemCheckService
     private function checkPhpVersion($version)
     {
         $state = version_compare(PHP_VERSION, $version, 'ge') ? 'success' : 'fail';
-        $label = sprintf($this->lang['syscheck_phpversion'], $version);
-        $stateLabel = $this->lang["syscheck_$state"];
-        return compact('state', 'label', 'stateLabel');
+        return [
+            'state' => $state,
+            'label' => sprintf($this->lang['syscheck_phpversion'], $version),
+            'stateLabel' => $this->lang["syscheck_$state"],
+        ];
     }
 
     /**
@@ -83,9 +85,11 @@ class SystemCheckService
     private function checkExtension($extension, $isMandatory = true)
     {
         $state = extension_loaded($extension) ? 'success' : ($isMandatory ? 'fail' : 'warning');
-        $label = sprintf($this->lang['syscheck_extension'], $extension);
-        $stateLabel = $this->lang["syscheck_$state"];
-        return compact('state', 'label', 'stateLabel');
+        return [
+            'state' => $state,
+            'label' => sprintf($this->lang['syscheck_extension'], $extension),
+            'stateLabel' => $this->lang["syscheck_$state"],
+        ];
     }
 
     /**
@@ -95,9 +99,11 @@ class SystemCheckService
     private function checkXhVersion($version)
     {
         $state = version_compare(CMSIMPLE_XH_VERSION, "CMSimple_XH $version", 'ge') ? 'success' : 'fail';
-        $label = sprintf($this->lang['syscheck_xhversion'], $version);
-        $stateLabel = $this->lang["syscheck_$state"];
-        return compact('state', 'label', 'stateLabel');
+        return [
+            'state' => $state,
+            'label' => sprintf($this->lang['syscheck_xhversion'], $version),
+            'stateLabel' => $this->lang["syscheck_$state"],
+        ];
     }
 
     /**
@@ -107,9 +113,11 @@ class SystemCheckService
     private function checkPlugin($plugin)
     {
         $state = is_dir("{$this->pluginsFolder}{$plugin}") ? 'success' : 'fail';
-        $label = sprintf($this->lang['syscheck_plugin'], $plugin);
-        $stateLabel = $this->lang["syscheck_$state"];
-        return compact('state', 'label', 'stateLabel');
+        return [
+            'state' => $state,
+            'label' => sprintf($this->lang['syscheck_plugin'], $plugin),
+            'stateLabel' => $this->lang["syscheck_$state"],
+        ];
     }
 
     /**
@@ -119,8 +127,10 @@ class SystemCheckService
     private function checkWritability($folder)
     {
         $state = is_writable($folder) ? 'success' : 'warning';
-        $label = sprintf($this->lang['syscheck_writable'], $folder);
-        $stateLabel = $this->lang["syscheck_$state"];
-        return compact('state', 'label', 'stateLabel');
+        return [
+            'state' => $state,
+            'label' => sprintf($this->lang['syscheck_writable'], $folder),
+            'stateLabel' => $this->lang["syscheck_$state"],
+        ];
     }
 }
